bb6729653abb2e050c43036347a46b72615a71c9
Sembiyan <sembiyan_arivazhagan@comcast.com>
2025-04-16 15:09:38 +0530

    Third commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,9 +7,9 @@ public class HelloWorld {
 
     public void saySecret() {
         for (int i = 0; i < 5; i++) {
-            System.out.println("Message: " + i);
+            System.out.println("Aft Rem: " + i);
         }
-        String secret = "qwertyuiopasdfghjklzxcvbnm";
+        String secret = "";
         System.out.println("This is a secret message: " + secret);
     }
 }

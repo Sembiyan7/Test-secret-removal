044b696d8bc29d93e99efbfa8b467a6aab476199
Sembiyan7 <69502199+Sembiyan7@users.noreply.github.com>
2025-04-16 15:15:10 +0530

    Fourth commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,5 @@
 public class HelloWorld {
 
-    // Method to print a greeting message
-    public void sayHello() {
-        System.out.println("Hello, World!");
-    }
-
     public void saySecret() {
         for (int i = 0; i < 5; i++) {
             System.out.println("Aft Rem: " + i);

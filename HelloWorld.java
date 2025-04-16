54e93e65377477466e964b18e3ee99bd05d355cf
Sembiyan <sembiyan_arivazhagan@comcast.com>
2025-04-16 16:19:24 +0530

    6th commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,12 +2,12 @@ public class HelloWorld {
 
     // Method to print a greeting message
     public void sayHello() {
-        System.out.println("Hello, jesus!");
-        System.out.println("Hello, Jesus!");
+        System.out.println("Hello, allah!");
+        System.out.println("Hello, Allah!");
     }
 
     public void saySecret() {
-        String secret = "qwerty==";
+        String secret = "";
         System.out.println("This is a secret message: " + secret);
     }
 }

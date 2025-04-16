0bc757f7051536b54cef0f5c75202ca2d1af8d99
Sembiyan <sembiyan_arivazhagan@comcast.com>
2025-04-16 14:59:30 +0530

    First commit

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+        // Create a new instance of the HelloWorld class
+        HelloWorld helloWorld = new HelloWorld();
+
+        // Call the sayHello method to print the greeting
+        helloWorld.saySecret();
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    // Method to print a greeting message
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }
+
+    public void saySecret() {
+        String secret = "qwertyuiopasdfghjklzxcvbnm";
+        System.out.println("This is a secret message: " + secret);
+    }
+}

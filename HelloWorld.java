9553bfd7a9d11331eeb737d6fe61b01c16ddd034
Sembiyan <sembiyan_arivazhagan@comcast.com>
2025-04-16 14:59:30 +0530

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    // Method to print a greeting message
+    public void sayHello() {
+        System.out.println("Hello, Universe!");
+    }
+
+    public void saySecret() {
+        String secret = "test_secret";
+        System.out.println("This is a secret message: " + secret);
+    }
+}
